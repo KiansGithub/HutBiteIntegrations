e00c9f5b19397e534d01b1fe6c6602500a3d4214
KiansGithub <kiansinnov8@outlook.com>
2025-10-28 09:03:58 +0000

    added router

--- app/routers/tables.py
+++ app/routers/tables.py
@@ -1,28 +1,21 @@
-# from fastapi import APIRouter, HTTPException, status 
-# from app.schemas.tables import Section
-# from app.services.tables import sections_service 
-# import logging 
+from fastapi import APIRouter, Depends, Query, HTTPException, status 
+from app.schemas.tables import Section 
+from app.services.tables import TableService 
+from app.core.deps import get_tables_service 
 
-# logger = logging.getLogger(__name__)
-# router = APIRouter(prefix="/tables", tags=[Sections])
+router = APIRouter(prefix="/tables", tags=["tables"])
 
-# @router.get("/sections", response_model=SectionsResponse)
-# async def sections():
-#     """
-#     Returns sections for the restaurant
-#     """
-#     try: 
-#         response = sections_service.get_sections
-
-#         if response.status == error:
-#             raise HTTPException(
-#                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#                 detail=f"Failed to retrieve sections"
-#             )
-#     except Exception as e:
-#         logger.error(f"Error in retrieving sections endpoint: {e}")
-#         raise HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             detail="Internal server error while retrieving sections"
-#         )
-
+@router.get("/sections", response_model=list[Section])
+async def get_sections(
+    store_id: str = Query(..., description="Store ID"),
+    menu_srv: str = Query(..., description="Menu_srv"),
+    database_name: str = Query(..., description="Database Name"), 
+    svc: TableService = Depends(get_tables_service)
+):
+    try:
+        return await svc.get_sections(store_id, menu_srv, database_name)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail=str(e)
+        )
