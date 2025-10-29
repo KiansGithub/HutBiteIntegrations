e0bad0140c96f42b35c615d7c9d06d1a1d010d0f
KiansGithub <kiansinnov8@outlook.com>
2025-10-29 11:46:22 +0000

    added client to menuservice initialization

--- app/services/menu.py
+++ app/services/menu.py
@@ -2,6 +2,9 @@ from fastapi import HTTPException, status
 from app.data.menus import MENU_ELCURIOSO
 
 class MenuService:
+
+    def __init__(self, http_client: httpx.AsyncClient):
+        self.http_client = http_client
     
     async def get_menu_items(
         self, 
